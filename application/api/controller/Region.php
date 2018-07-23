67a39d0016148d5736940add50a3f0ad9a4a3c52
shengli <363912809@qq.com>
2018-07-23 17:05:01 +0800

    region

--- application/api/controller/Region.php
+++ application/api/controller/Region.php
@@ -8,7 +8,7 @@ class Region extends Base {
 
 	public function __construct(){
 		// 设置所有方法的默认请求方式
-		$this->method = 'get';
+		$this->method = 'POST';
 
 		parent::__construct();
 	}
@@ -22,8 +22,7 @@ class Region extends Base {
   			$data[$region['code']] = $region;
   		}
 
-      $data = $this->_tree($data);
-      p($data);
+  		$data = $this->_tree($data);
   		response_success($data);
     }
     
@@ -36,11 +35,11 @@ class Region extends Base {
 
    		$tree = array();
    		foreach ($data as $item) {
-         if(isset($data[$item['parentCode']])){
-            $data[$item['parentCode']]['sub'][] = &$data[$item['code']];
-         } else {
-            $tree[] = &$data[$item['code']];
-         }
+               if(isset($data[$item['parentCode']])){
+                  $data[$item['parentCode']]['sub'][] = &$data[$item['code']];
+               } else {
+                  $tree[] = &$data[$item['code']];
+               }
    		}
 
    		return $tree;
