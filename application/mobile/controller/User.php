653c619520e6e6806cafd6938584db30021348df
shengli <363912809@qq.com>
2018-07-24 17:26:00 +0800

    dynamic

--- application/mobile/controller/User.php
+++ application/mobile/controller/User.php
@@ -1,29 +1,11 @@
 <?php
-/**
- * tpshop
- * ============================================================================
- * * 版权所有 2015-2027 深圳搜豹网络科技有限公司，并保留所有权利。
- * 网站地址: http://www.tp-shop.cn
- * ----------------------------------------------------------------------------
- * 这不是一个自由软件！您只能在不用于商业目的的前提下对程序代码进行修改和使用 .
- * 不允许对程序代码以任何形式任何目的的再发布。
- * 采用最新Thinkphp5助手函数特性实现单字母函数M D U等简写方式
- * ============================================================================
- * 2015-11-21
- */
+
 namespace app\mobile\controller;
 
-use app\common\logic\CartLogic;
-use app\common\logic\MessageLogic;
-use app\common\logic\UsersLogic;
-use app\common\logic\OrderLogic;
-use app\common\logic\CouponLogic;
-use think\Page;
-use think\Request;
-use think\Verify;
+
 use think\db;
 
-class User extends MobileBase
+class User extends Base
 {
 
     public $user_id = 0;
@@ -75,6 +57,7 @@ class User extends MobileBase
      */
     public function index()
     {
+        die('我的页面');
         $user_id =$this->user_id;
         $logic = new UsersLogic();
         $user = $logic->get_info($user_id); //当前登录用户信息
