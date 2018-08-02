ada08dcd8d65814e5f64a0e83901a5218a071dad
shengli <363912809@qq.com>
2018-08-02 14:47:52 +0800

    user

--- application/api/controller/User.php
+++ application/api/controller/User.php
@@ -452,4 +452,55 @@ class User extends Base {
 
         response_success($lists);
     }
+
+    /**
+     * [friend 我的好友]
+     * @return [type] [description]
+     */
+    public function friend(){
+        $user_id = I('user_id', 1);
+
+        $lists = M('friend')->alias('f')
+            ->join('users u', 'f.friend_id = u.user_id', 'left')
+            ->where('f.user_id', $user_id)
+            ->where('twoway', '1')
+            ->order('f.id desc')
+            ->field('friend_id user_id, head_pic, nickname, auth_video_status')
+            ->select();
+
+        response_success($lists);
+    }
+
+    /**
+     * [friend 关注和粉丝]
+     * @param type 1 关注 2 粉丝
+     * @return [type] [description]
+     */
+    public function attentionFans(){
+        $user_id = I('user_id', 1);
+        $type = I('type', 1);
+        $page = I('page', 1);
+
+
+        if($type == '1'){
+            $join_on = 'f.friend_id = u.user_id';
+            $where['f.user_id'] = $user_id;
+            $field = 'friend_id user_id, head_pic, nickname, auth_video_status, twoway';
+        } else {
+            $join_on = 'f.friend_id = u.user_id';
+            $where['f.friend_id'] = $user_id;
+            $field = 'user_id, head_pic, nickname, auth_video_status, twoway';
+        }
+
+        $limit_start = ($page-1)*20;
+        $lists = M('friend')->alias('f')
+            ->join('users u', $join_on, 'left')
+            ->where($where)
+            ->order('f.id desc')
+            ->field($field)
+            ->limit($limit_start, 20)
+            ->select();
+
+        response_success($lists);
+    }
 }
