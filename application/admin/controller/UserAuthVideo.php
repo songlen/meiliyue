f1e1f41b0eb4648bfe9638db59b6b57d9524a791
shengli <363912809@qq.com>
2018-08-09 16:44:58 +0800

    auth video

--- application/admin/controller/UserAuthVideo.php
+++ application/admin/controller/UserAuthVideo.php
@@ -12,4 +12,32 @@ class UserAuthVideo extends Base {
     	
         return $this->fetch();
     }
+
+    public function ajaxindex(){
+        // 搜索条件
+        $condition = array();
+        I('mobile') ? $condition['mobile'] = I('mobile') : false;
+        I('email') ? $condition['email'] = I('email') : false;
+               
+        $model = M('UsersAuthVideo');
+        $count = $model->where($condition)->count();
+        $Page  = new AjaxPage($count,10);
+        //  搜索条件下 分页赋值
+        foreach($condition as $key=>$val) {
+            $Page->parameter[$key]   =   urlencode($val);
+        }
+        
+        $lists = $model->alias('uav')->where($condition)
+        	->join('users u', 'uav.user_id=u.user_id', 'left')
+        	->order('id desc')
+        	->limit($Page->firstRow.','.$Page->listRows)
+        	->field('u.user_id, u.nickname, u.uuid, uav.id, uav.auth_video_url, uav.status, uav.add_time')
+        	->select();
+                           
+        $show = $Page->show();
+        $this->assign('lists',$lists);
+        $this->assign('page',$show);// 赋值分页输出
+        $this->assign('pager',$Page);
+        return $this->fetch();
+    }
  }
