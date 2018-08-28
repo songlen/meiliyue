5faadc21ba998afed89ecf85024b92a8ed005fac
shengli <363912809@qq.com>
2018-08-28 09:03:39 +0800

    vip

--- application/api/controller/Vip.php
+++ application/api/controller/Vip.php
@@ -80,7 +80,7 @@ class Vip extends Base {
 		if($paymentMethod == 'alipay'){
 			$AlipayLogic = new AlipayLogic();
 			$orderStr = $AlipayLogic->generateOrderStr($order_no, $order);
-			response_success(array('orderStr' => $orderStr));
+			response_success($orderStr);
 		}
 	}
 
