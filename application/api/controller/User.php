130d29ba4bf62affc86eb75b15cd4f0e9ca2c6fe
shengli <363912809@qq.com>
2018-08-29 14:04:42 +0800

    auth identity

--- application/api/controller/User.php
+++ application/api/controller/User.php
@@ -204,11 +204,12 @@ class User extends Base {
         $count = M('identity_auth')->where('user_id', $user_id)->count();
         if($count){
             M('identity_auth')->where('user_id', $user_id)->update($data);
-            response_success('', '操作成功');
+            
         } else {
             M('identity_auth')->insert($data);
-            response_error('', '操作失败');
         }
+        M('users')->where('user_id', $user_id)->update(array('auth_identity_status'=>1));
+        response_success('', '操作成功');
     }
 
     // 关注
