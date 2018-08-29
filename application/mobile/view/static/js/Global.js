b29de020adfccc93145cf4b9bf826a97881a9652
jiqishoubi <437753880@qq.com>
2018-08-29 11:27:06 +0800

    global.js压缩图片方法

--- application/mobile/view/static/js/Global.js
+++ application/mobile/view/static/js/Global.js
@@ -229,6 +229,72 @@ let Global = (function () {
         return srcTemp;
     }
 
+    //压缩图片的方法
+    function compressImg(file,callback){
+        let reader = new FileReader();
+        let img = new Image();
+        //canvas
+        let canvas = document.createElement('canvas');
+        let context = canvas.getContext('2d');
+
+        //图片加载完毕后
+        img.onload = function () {
+            // 图片原始尺寸
+            var originWidth = this.width;
+            var originHeight = this.height;
+            // 最大尺寸限制
+            var maxWidth = 400, maxHeight = 400;
+            // 目标尺寸
+            var targetWidth = originWidth, targetHeight = originHeight;
+            // 图片尺寸超过400x400的限制
+            if (originWidth > maxWidth || originHeight > maxHeight) {
+                if (originWidth / originHeight > maxWidth / maxHeight) {
+                    // 更宽，按照宽度限定尺寸
+                    targetWidth = maxWidth;
+                    targetHeight = Math.round(maxWidth * (originHeight / originWidth));
+                } else {
+                    targetHeight = maxHeight;
+                    targetWidth = Math.round(maxHeight * (originWidth / originHeight));
+                }
+            }
+                
+            // canvas对图片进行缩放
+            canvas.width = targetWidth;
+            canvas.height = targetHeight;
+            // 清除画布
+            context.clearRect(0, 0, targetWidth, targetHeight);
+            // 图片压缩
+            context.drawImage(img, 0, 0, targetWidth, targetHeight);
+            // canvas转为blob并上传
+            canvas.toBlob(function (blob) {
+                // // 图片ajax上传
+                // var xhr = new XMLHttpRequest();
+                // // 文件上传成功
+                // xhr.onreadystatechange = function() {
+                //     if (xhr.status == 200) {
+                //         // xhr.responseText就是返回的数据
+                //     }
+                // };
+                // // 开始上传
+                // xhr.open("POST", 'upload.php', true);
+                // xhr.send(blob);  
+                
+                callback(blob);
+
+            }, file.type || 'image/png');
+        };
+
+        // 文件base64化，以便获知图片原始尺寸 ，将file转成img的src
+        reader.onload = function(e) {
+            img.src = e.target.result;
+        };
+
+        //init触发
+        if(file.type.indexOf("image")>-1){
+            reader.readAsDataURL(file);   
+        }
+    }
+
     //Global暴露的接口------------------------
     return {
         //值
@@ -248,7 +314,8 @@ let Global = (function () {
         fullScreen, // 预览全屏视频 ele:video元素
         fullScreenAuth, //预览全屏认证小视频
         fullScreenImg, //全屏显示图片 src
-        filterHttpImg //过滤http头像img  src
+        filterHttpImg, //过滤http头像img  src
+        compressImg //压缩图片，file callback(blob)
     }
 })();
 
