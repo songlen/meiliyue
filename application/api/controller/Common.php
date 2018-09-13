24d2cd34b7661a272f5044d2f84d3cf057220b40
shengli <363912809@qq.com>
2018-09-13 17:25:51 +0800

    retate

--- application/api/controller/Common.php
+++ application/api/controller/Common.php
@@ -48,16 +48,16 @@ class Common extends Base {
 
 
     // 旋转图片
-    public function rotate($filepath, $degrees = 90){
+    public function rotate(){
         $filepath  = I('filepath');
         $degrees  = I('degrees', 90);
 
 
         if(!file_exists($filepath)) return false;
 
-        // $Image = \think\Image::open($filepath);
-        // // 对图像使用默认的顺时针旋转90度操作
-        // $Image->rotate($degrees)->save($filepath);
+        $Image = \think\Image::open($filepath);
+        // 对图像使用默认的顺时针旋转90度操作
+        $Image->rotate($degrees)->save($filepath);
 
         response_success();
     }
