2d615486fe0a77f8a67802ad7a6c17c05d4f6395
shengli <363912809@qq.com>
2018-10-08 23:21:47 +0800

    vip

--- application/admin/controller/Vip.php
+++ application/admin/controller/Vip.php
@@ -70,4 +70,55 @@ class Vip extends Base {
         $this->assign('total_amount', $total_amount);
         return $this->fetch();
     }
+
+    public function export(){
+        $strTable ='<table width="500" border="1">';
+        $strTable .= '<tr>';
+        $strTable .= '<td style="text-align:center;font-size:12px;" width="100">会员昵称</td>';
+        $strTable .= '<td style="text-align:center;font-size:12px;width:80px;">uuid</td>';
+        $strTable .= '<td style="text-align:center;font-size:12px;" width="*">会员级别</td>';
+        $strTable .= '<td style="text-align:center;font-size:12px;" width="*">支付时间</td>';
+        $strTable .= '</tr>';
+        $count = M('vip_order')->where('paystatus', 1)->count();
+        $p = ceil($count/5000);
+        for($i=0;$i<$p;$i++){
+            $start = $i*5000;
+            $end = ($i+1)*5000;
+            $list = M('vip_order')->alias('vo')
+                ->join('users u', 'vo.user_id=u.user_id', 'left')
+                ->where('vo.paystatus', 1)
+                ->order('vo.id desc')
+                ->limit($start.','.$end)
+                ->select();
+            if(is_array($list)){
+                foreach($list as $k=>$val){
+                    
+                    switch ($val['level']) {
+                        case 1:
+                            $level_name = '白银VIP';
+                            break;
+                        case 2:
+                            $level_name = '黄金VIP';
+                            break;
+                        case 3:
+                            $level_name = '铂金VIP';
+                            break;
+                        case 4:
+                            $level_name = '钻石VIP';
+                            break;
+                    }
+                    $strTable .= '<tr>';
+                    $strTable .= '<td style="text-align:center;font-size:12px;">'.$val['nickname'].'</td>';
+                    $strTable .= '<td style="text-align:left;font-size:12px;">'.$val['uuid'].' </td>';
+                    $strTable .= '<td style="text-align:left;font-size:12px;">'.$level_name.'</td>';
+                    $strTable .= '<td style="text-align:left;font-size:12px;">'.date('Y-m-d H:i',$val['paytime']).'</td>';
+                    $strTable .= '</tr>';
+                }
+                unset($list);
+            }
+        }
+        $strTable .='</table>';
+        downloadExcel($strTable,'vip购买记录');
+        exit();
+    }
  }
