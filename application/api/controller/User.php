d4a107fa283074c2a3adb588b808ca9db8b3a5c6
shengli <363912809@qq.com>
2018-10-09 11:51:20 +0800

    me

--- application/api/controller/User.php
+++ application/api/controller/User.php
@@ -400,7 +400,10 @@ class User extends Base {
             }
 
             // 发消息
-
+            $userinfo = Db::name('users')->where('user_id', $user_id)->field('nickname')->find();
+            $MessageLogic = new MessageLogic();
+            $message = $userinfo['nickname'].'关注了你';
+            $MessageLogic->add($user_id, $message);
 
             response_success('', '关注成功');
         } else {
